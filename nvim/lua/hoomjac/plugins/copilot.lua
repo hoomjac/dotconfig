076423d25b57a3b44cda1d076e7b75506b3035fd
Lin Hongjie <hoomjac@gmail.com>
2024-09-13 12:04:46 +0800

    手动指定Copilot的node versioon（>18）

--- nvim/lua/hoomjac/plugins/copilot.lua
+++ nvim/lua/hoomjac/plugins/copilot.lua
@@ -5,6 +5,7 @@ return {
 	opts = {
 		suggestion = { enabled = false },
 		panel = { enabled = false },
+		copilot_node_command = vim.fn.expand("$HOME") .. "/.volta/tools/image/node/22.3.0/bin/node", -- Node.js version must be > 18.x
 	},
 	config = true,
 }
